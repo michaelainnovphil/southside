a82752865f56345ea0961a4fbcfb299d5598581c
michaelainnovphil <mdbarreda@innovphil.com>
2025-04-02 11:03:56 +0800

    updated text

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
+import React from "react";
+import "./globals.css";
 
-export default function Home() {
+const App = () => {
+
   return (
-    <div className="bg-[url(/image1.jpg)] bg-auto md:bg-contain}">
-      <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="logo"
-          src="/logo.png"
-          alt="logo"
-          width={180}
-          height={38}
-          priority
-        />
-    </div>
+    <div className="bg-[url(/image1.jpg)] bg-cover">
       
+      
+
       <div className="app-container">
         <div className="boxes-container">
 
@@ -25,16 +20,6 @@ export default function Home() {
           height={38}
           priority
         />
-
-        <Image
-          className="image2"
-          src="/image2.jpg"
-          alt="image2"
-          width={180}
-          height={38}
-          priority
-        />
-
         <Image
           className="image3"
           src="/image3.jpg"
@@ -43,9 +28,42 @@ export default function Home() {
           height={38}
           priority
         />
+        <Image
+          className="image3"
+          src="/image3.jpg"
+          alt="image3"
+          width={180}
+          height={38}
+          priority
+        />
+        <Image
+          className="image2"
+          src="/image2.jpg"
+          alt="image2"
+          width={180}
+          height={38}
+          priority
+        />
+        
+        
+      </div>
+      </div>
 
+      
+      <div className="txt1">
+        <p className="text-l text-[#ffffff] tracking-wide font-regular"><i>“Yesterday I advised you not to write me every day, I still hold the same opinion today and it would be very good for both of us, and so I repeat my advice today even more emphatically only please, Milena, don't listen to me, and write me every day anyway, it can even be very brief, briefer than today's letters, just 2 lines, just one, just one word, but if I had to go without them I would suffer terribly.”
+        ― Franz Kafka, Letters to Milena</i></p>
       </div>
+      
+      <div className="containerbot">
+        
       </div>
-    </div>
+          
+    </div> 
+
+ 
+
   );
-}
+};
+
+export default App;
